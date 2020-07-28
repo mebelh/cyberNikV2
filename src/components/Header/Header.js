84688eded2116574cda81e3b21ac7e5dace9127c
artif1c1al <zhuravlenkoartem@gmail.com>
2020-07-28 11:49:12 +0300

    hz commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,17 +11,23 @@ import "./Header.scss";
 
 const Greetings = ({ name }) => {
     let time = new Date()
+    setInterval(() => { time = new Date() }, 3600)
+
     let hours = time.getHours();
+    let ans = '';
 
     if (hours > 0 && hours < 6) {
-        return <span>Доброй ночи {name}</span>
+        ans = `Доброй ночи ${name}`
     } else if (hours > 6 && hours < 12) {
-        return <span>Доброе утро {name}</span>
+        ans = `Доброе утро ${name}`
     } else if (hours > 12 && hours < 18) {
-        return <span>Добрый день {name}</span>
+        ans = `Добрый день ${name}`
     } else if (hours > 18 && hours < 24) {
-        return <span>Добрый вечер {name}</span>
+        ans = `Добрый вечер ${name}`
     }
+
+    return <div style={{ color: '#555' }}>{ans}</div>
+
 }
 
 export default function Header() {
@@ -36,8 +42,8 @@ export default function Header() {
                 <DropdownBtn className="btn header__btn" />
             </div>
 
-            <Greetings name={"Artem"} />
             <div className="header_right-part">
+                <Greetings name={"Artem"} />
                 <SignBtn
                     label="Войти"
                     href="/auth/login"
