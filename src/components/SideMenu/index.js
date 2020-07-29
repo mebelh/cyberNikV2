0b68c4aafe0dc9e9857b51480f46ef24e68cf057
artif1c1al <zhuravlenkoartem@gmail.com>
2020-07-29 23:48:52 +0300

    new cool features

--- src/components/SideMenu/index.js
+++ src/components/SideMenu/index.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import './style.scss'
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 
 export default function SideMenu_course() {
   return (
-    <React.Fragment>
-      <div class="accordion" id="accordionExample">
-        <div class="card">
-          <div class="card-header" id="headingOne">
-            <h2 class="mb-0">
-              <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+    <div className="sideMenu">
+      <div className="accordion" id="accordionExample">
+        <div className="card">
+          <div className="card-header" id="headingOne">
+            <h2 className="mb-0">
+              <button className="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                 Модуль 1
-        </button>
+              </button>
             </h2>
           </div>
 
-          <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-            <div class="card-body">
+          <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+            <div className="card-body">
               <a className="lecture" href="#">
                 <div className="lecture__name">О защите</div>
                 <div className="lecture__duration">49 минут</div>
@@ -33,41 +34,57 @@ export default function SideMenu_course() {
             </div>
           </div>
         </div>
-        <div class="card">
-          <div class="card-header" id="headingTwo">
-            <h2 class="mb-0">
-              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        <div className="card">
+          <div className="card-header" id="headingTwo">
+            <h2 className="mb-0">
+              <button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
                 Модуль 2
-        </button>
+              </button>
             </h2>
           </div>
-          <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-            <div class="card-body" href="#">
+          <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+            <div className="card-body" href="#">
               <a className="lecture" href="#">
-                <div className="lecture-name">О защите</div>
-                <div className="lecture-duration">35 минут</div>
+                <div className="lecture__name">О защите</div>
+                <div className="lecture__duration">49 минут</div>
+              </a>
+              <a className="lecture" href="#">
+                <div className="lecture__name">О защите</div>
+                <div className="lecture__duration">55 минут</div>
+              </a>
+              <a className="lecture" href="#">
+                <div className="lecture__name">О защите</div>
+                <div className="lecture__duration">45 минут</div>
               </a>
             </div>
           </div>
         </div>
-        <div class="card">
-          <div class="card-header" id="headingThree">
-            <h2 class="mb-0">
-              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        <div className="card">
+          <div className="card-header" id="headingThree">
+            <h2 className="mb-0">
+              <button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                 Модуль 3
-        </button>
+              </button>
             </h2>
           </div>
-          <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-            <a class="card-body" href="#">
+          <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+            <div className="card-body" href="#">
               <a className="lecture" href="#">
-                <div className="lecture-name">О защите</div>
-                <div className="lecture-duration">45 минут</div>
+                <div className="lecture__name">О защите</div>
+                <div className="lecture__duration">49 минут</div>
               </a>
-            </a>
+              <a className="lecture" href="#">
+                <div className="lecture__name">О защите</div>
+                <div className="lecture__duration">55 минут</div>
+              </a>
+              <a className="lecture" href="#">
+                <div className="lecture__name">О защите</div>
+                <div className="lecture__duration">45 минут</div>
+              </a>
+            </div>
           </div>
         </div>
       </div>
-    </React.Fragment>
+    </div>
   )
 }
