a1c549dfaa98663d8933292576454f0bc03d470f
mebelh <memetbelualov@bk.ru>
2020-07-31 14:38:33 +0300

    mod nm

--- src/components/pages/AddCourse/AddModules/ModuleGroupe/Module/index.js
+++ src/components/pages/AddCourse/AddModules/ModuleGroupe/Module/index.js
@@ -29,7 +29,7 @@ export default function Module({ onModuleChange, num }) {
                     <input
                         type="text"
                         name="name"
-                        value={module.duration}
+                        value={module.name}
                         onChange={({ target }) => {
                             changeModule("name", target.value);
                         }}
